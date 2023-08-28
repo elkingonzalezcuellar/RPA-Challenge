7e999e26be2eb5dee964f9e71abd24eb902d221d
egonzalez <egonzalez@palcort.com>
2023-08-28 18:26:30 -0500

    fix : testing

--- nytimes_robot.py
+++ nytimes_robot.py
@@ -78,7 +78,7 @@ class NYTimesRobot:
             print("Error while clicking search button:", e)
 
         try:
-            self.browser.wait_until_page_contains_element("css=[data-testid='search-input']", timeout=10)
+            self.browser.wait_until_page_contains_element("css=[data-testid='search-input']", timeout=20)
             self.browser.input_text("css=[data-testid='search-input']", search_phrase)
         except Exception as e:
             print("i cant search phrase")
