bc0ee0befec6b6df87cec32585023702a7bb74eb
egonzalez <egonzalez@palcort.com>
2023-08-28 22:31:46 -0500

    test

--- nytimes_robot.py
+++ nytimes_robot.py
@@ -100,7 +100,7 @@ class NYTimesRobot:
             print("Failed to capture screenshot:", e)
         
         try:
-            self.browser.wait_until_element_is_not_visible("css=.css-et84d3.e1ejehf01")
+            self.browser.wait_until_element_is_not_visible("css=.css-et84d3.e1ejehf01", timeout=20)  # Increase the timeout value if needed
             self.browser.click_element("css=[data-testid='nav-button']")
         except Exception as e:
             print("Failed to click the navigation button:", e)
